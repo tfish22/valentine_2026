5686bceda215e69adb68e53dfd1510abae6893db
tfish22 <timothyfish@vt.edu>
2026-02-09 23:13:07 -0500

    Add playing gif on click ability

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // script.js
 
+// Function to replay a GIF by resetting its src
+function replayGif(imgElement) {
+    var src = imgElement.src;
+    imgElement.src = '';
+    imgElement.src = src;
+}
+
 // Function to display the cat.gif
 function displayCat() {
     // Get the container
@@ -9,6 +16,11 @@ function displayCat() {
     var catImage = document.createElement('img');
     catImage.src = 'i-love-you-i-love-you-so-much.gif';
     catImage.alt = 'Cat';
+
+    // Add click event to replay the GIF
+    catImage.onclick = function() {
+        replayGif(catImage);
+    };
     
     // Add it to the page
     imageContainer.appendChild(catImage);
